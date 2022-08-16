06979e2f3726af02f13301540465a897905c4e0e
megu3k <49593780+megu3k@users.noreply.github.com>
2022-08-16 10:10:59 +0900

    section4 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import { useState, useCallback } from "react";
-import { ChildArea } from "./childarea";
-import "./styles.css";
+import { InlineStyle } from "./components/InlineStyle";
+import "style.css";
+import { CssModules } from "./components/CssModules";
+import { StyledJsx } from "./components/StyledJsx";
+import { StyledComponents } from "./components/StyledComponents";
+import { Emotion } from "./components/Emotion";
 
 export default function App() {
-  console.log("App");
-  const [text, setText] = useState("");
-  const [open, setOpen] = useState(false);
-
-  const onChangeText = (e) => setText(e.target.value);
-
-  const onClickOpen = () => setOpen(!open);
-
-  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
-
   return (
     <div className="App">
-      <input value={text} onChange={onChangeText} />
-      <br />
-      <br />
-      <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} onClickClose={onClickClose} />
+      <InlineStyle />
+      <CssModules />
+      <StyledJsx />
+      <StyledComponents />
+      <Emotion />
     </div>
   );
 }
